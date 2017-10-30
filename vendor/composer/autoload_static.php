c8439bdb0568bac845abbfa65ba1e17f63f6b90c
qinyuguang <qinyuguang@gmail.com>
2017-10-30 18:21:52 +0800

    add mailer

--- vendor/composer/autoload_static.php
+++ vendor/composer/autoload_static.php
@@ -10,6 +10,7 @@ class ComposerStaticInita7117a867fd7e3710695cab4876c089c
         '0e6d7bf4a5811bfa5cf40c5ccd6fae6a' => __DIR__ . '/..' . '/symfony/polyfill-mbstring/bootstrap.php',
         'c964ee0ededf28c96ebd9db5099ef910' => __DIR__ . '/..' . '/guzzlehttp/promises/src/functions_include.php',
         'a0edc8309cc5e1d60e3047b5df6b7052' => __DIR__ . '/..' . '/guzzlehttp/psr7/src/functions_include.php',
+        '2c102faa651ef8ea5874edb585946bce' => __DIR__ . '/..' . '/swiftmailer/swiftmailer/lib/swift_required.php',
         '37a3dc5111fe8f707ab4c132ef1dbc62' => __DIR__ . '/..' . '/guzzlehttp/guzzle/src/functions_include.php',
     );
 
